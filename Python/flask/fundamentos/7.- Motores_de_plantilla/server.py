9655f20b1c81bb46571ec71fd535c84abc4ebe58
Fabrizio226 <fabriziomendieta@liceovvh.cl>
2026-07-15 12:27:57 -0400

    WWW

--- Python/flask/fundamentos/7.- Motores_de_plantilla/server.py
+++ Python/flask/fundamentos/7.- Motores_de_plantilla/server.py
@@ -2,36 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def inicio():
-
     return render_template(
 
         "index.html",
 
         nombre="Fabrizio",
-
-        apellido="",
-
-        curso="",
-
-        institucion="",
-
-        profesor=False,
-
+        apellido="Ortiz",
+        curso="4° Programación",
+        institucion="Liceo Comercial Vate Vicente Huidobro",
         anio=2026,
+        es_docente= False,  #Con true cambia el mensjae
+        tecnologias=[
+            "Python",
+            "Flask",
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ]
+    )
 
-lenguajes = [
 
-    "Python",
-
-    "Flask",
-
-    "HTML",
-
-    "CSS",
-
-    "JavaScript"
-
-]
-)
+if __name__ == "__main__":
+    app.run(debug=True)
